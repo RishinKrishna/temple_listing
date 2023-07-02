68c82ebb95c96f2e3377aca27dfe0b5029d5daed
RishinKrishna <rishinkrishna07@gmail.com>
2023-07-02 20:29:44 +0530

    changed the scripts of bootstrap to app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import Script from "next/script";
 export default function App({ Component, pageProps }) {
   let getPage = Component.getLayout ? ()=> Component.getLayout(<Component {...pageProps}/>) : ()=> <Component {...pageProps}/>;
   return (
     <>
       {getPage()}
       <ToastContainer />
+      <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></Script>
+      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></Script>
     </>
   );
 }
